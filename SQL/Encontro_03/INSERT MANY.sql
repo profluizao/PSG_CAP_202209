6870b96fb633cad80038d71be424ebc3de130ae9
Prof. Luiz Augusto Rodrigues <90289697+profluizao@users.noreply.github.com>
2022-09-08 17:22:11 -0400

    Create INSERT MANY.sql

---
+++ SQL/Encontro_03/INSERT MANY.sql
@@ -0,0 +1,11 @@
+INSERT INTO Cliente(Nome,Documento,Telefone,Email,Endereco)
+VALUES
+('Mateus','123','987654321','mateus@teste.com','Rua x, n. 10'),
+('Lucas','456','987654321','lucas@teste.com','Rua x, n. 11'),
+('João','789','987321654','joao@teste.com','Rua x, n. 12'),
+('Pedro','987','987321987','pedro@teste.com','Rua x, n. 13'),
+('Tiago','741','987987987','tiago@teste.com','Rua x, n. 14'),
+('Judas','258','963852741','judas@teste.com','Rua x, n. 15'),
+('Marcos','753','958745213','marcos@teste.com','Rua x, n. 16'),
+('André','761','932165485','andre@teste.com','Rua x, n. 17')
+GO
