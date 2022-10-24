525bb002e4e4e1006f4530bbd619f1d19dd65f67
Prof. Luiz Augusto Rodrigues <prof.luiz.a.rodrigues.2018@gmail.com>
2022-10-24 14:45:23 -0400

    Atualização Encontro 32
    Dados no FakeDB.

--- CSharp/EstoqueSolucao/Atacado.Dominio/Estoque/Produto.cs
+++ CSharp/EstoqueSolucao/Atacado.Dominio/Estoque/Produto.cs
@@ -9,10 +9,13 @@ namespace Atacado.Dominio.Estoque
 {
     public class Produto : BaseEstoque
     {
+        private int codigoCategoria;
+
         private int codigoSubcategoria;
 
+        public int CodigoCategoria { get => codigoCategoria; set => codigoCategoria = value; }
         public int CodigoSubcategoria { get => this.codigoSubcategoria; set => this.codigoSubcategoria = value; }
-
+        
         public Produto(int codigoSubcategoria) : base()
         {
             this.codigoSubcategoria = codigoSubcategoria;
@@ -22,6 +25,13 @@ namespace Atacado.Dominio.Estoque
             : base(codigo, descricao, ativo, dataInclusao)
         {
             this.codigoSubcategoria = codigoSubcategoria;
+        }
+
+        public Produto(int codigo, int codigoSubcategoria, int codigoCategoria, string descricao, DateTime dataInclusao)
+            : base(codigo, descricao, true, dataInclusao)
+        {
+            this.codigoSubcategoria = codigoSubcategoria;
+            this.codigoCategoria = codigoCategoria;
         }
     }
 }
