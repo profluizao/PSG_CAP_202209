28418968348453df4986393deb0c6ad0d63ab440
Prof. Luiz Augusto Rodrigues <prof.luiz.a.rodrigues.2018@gmail.com>
2022-11-17 16:09:23 -0400

    Atualização Encontro 47

---
+++ CSharp/EstoqueSolucao/Atacado.DB.EF/Database/MeuContexto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atacado.DB.EF.Database
+{
+    public partial class MeuContexto : DbContext
+    {
+        public MeuContexto()
+        { }
+
+        public MeuContexto(DbContextOptions<MeuContexto> options)
+            : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
