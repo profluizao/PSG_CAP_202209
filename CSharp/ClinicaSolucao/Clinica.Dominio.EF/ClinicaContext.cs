f67c03e94473a3e96928e0e90033a886b7469aae
Prof. Luiz Augusto Rodrigues <prof.luiz.a.rodrigues.2018@gmail.com>
2022-11-22 17:27:53 -0400

    Adicionando novo projeto ClinicaApi.

---
+++ CSharp/ClinicaSolucao/Clinica.Dominio.EF/ClinicaContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica.Dominio.EF
+{
+    public partial class ClinicaContext : DbContext
+    {
+        public DbSet<Paciente> Pacientes { get; set; } = null!;
+
+        public ClinicaContext() : base()
+        { }
+
+        public ClinicaContext(DbContextOptions<ClinicaContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Paciente>(entity => 
+            {
+                entity.Property(e => e.Situacao).HasDefaultValueSql("((1))");
+                entity.Property(e => e.DataInclusao).HasDefaultValueSql("(getdate())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
